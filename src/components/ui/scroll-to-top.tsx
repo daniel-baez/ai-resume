f4ccd909d901fe52f078560d47800133c58bb3e1
Daniel Baez <daplay@gmail.com>
2024-12-29 21:25:54 -0600

    scroll to top

--- src/components/ui/scroll-to-top.tsx
+++ src/components/ui/scroll-to-top.tsx
@@ -31,14 +31,14 @@ export function ScrollToTop() {
     })
   }
 
-  return (
+  /* return (
     <>
       {isVisible && (
         <Button
           id="scroll-to-top"
           onClick={scrollToTop}
-          className="fixed bottom-8 right-8 p-2 rounded-full bg-blue-600 hover:bg-blue-700 text-white shadow-lg z-50"
-          // className="h-14 w-14 rounded-full shadow-lg bg-blue-600/40 hover:bg-blue-700/50 text-white backdrop-blur-[2px]
+          // className="fixed bottom-8 right-8 p-2 rounded-full bg-blue-600 hover:bg-blue-700 text-white shadow-lg z-50"
+          className="h-14 w-14 rounded-full shadow-lg bg-blue-600/40 hover:bg-blue-700/50 text-white backdrop-blur-[2px]
 // -          transition-all duration-300 hover:scale-105 active:scale-95"
           size="icon"
           aria-label="Scroll to top"
@@ -47,5 +47,22 @@ export function ScrollToTop() {
         </Button>
       )}
     </>
+  ) */
+
+  return (
+    <div className={`md:hidden fixed bottom-16 right-6 z-50 transition-opacity duration-300 ${
+        isVisible ? 'opacity-80' : 'opacity-0 pointer-events-none'
+      }`}>
+        <Button
+          variant="default"
+          size="icon"
+          className="h-14 w-14 rounded-full shadow-lg bg-blue-600/40 hover:bg-blue-700/50 text-white backdrop-blur-[2px]
+            transition-all duration-300 hover:scale-105 active:scale-95"
+          onClick={scrollToTop}
+          aria-label="Scroll to top"
+        >
+          <ArrowUp strokeWidth={3} size={30} className="h-8 w-8" />
+        </Button>
+      </div>
   )
 }
