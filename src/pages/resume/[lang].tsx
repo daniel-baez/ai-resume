1813508e1e57895442a2131e2d9ca6b04b7d3f91
Daniel Baez <daplay@gmail.com>
2025-05-17 15:00:18 -0500

    x

--- src/pages/resume/[lang].tsx
+++ src/pages/resume/[lang].tsx
@@ -2,10 +2,18 @@
 import fs from 'fs';
 import path from 'path';
 import { GetServerSideProps } from 'next';
+import { AVAILABLE_LANGUAGES } from '@/constants/i18n'; // Adjust the import path as necessary
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const { lang } = context.params as { lang: string };
   const language = lang || 'en'; // Default to English if no language is provided
+  
+  // only allow languages defined in AVAILABLE_LANGUAGES
+  if (!Object.keys(AVAILABLE_LANGUAGES).includes(language)) {
+    return {
+      notFound: true
+    };
+  }
 
   // Vérifier si le PDF statique existe
   const staticPath = path.join(process.cwd(), 'public', 'pdfs', `resume-${language}.pdf`);
@@ -13,7 +21,9 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   if (fs.existsSync(staticPath)) {
     const pdfBuffer = fs.readFileSync(staticPath);
     context.res.setHeader('Content-Type', 'application/pdf');
-    context.res.setHeader('Content-Disposition', `inline; filename="resume-daniel-baez-${language}.pdf"`);
+    const date = new Date();
+    const yearMonth = `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}`;
+    context.res.setHeader('Content-Disposition', `inline; filename="resume-daniel-baez-${language}-${yearMonth}.pdf"`);
     context.res.end(pdfBuffer);
     return { props: {} };
   }
