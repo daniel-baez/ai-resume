e13e3c89a0a2936d5be0418587f710fdd610bcc2
Daniel Baez <daplay@gmail.com>
2025-12-15 21:43:48 -0600

    WIP

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -12,7 +12,6 @@ export const getProfileData = (lang: Language): ProfileData => {
   // Load the language-specific profile data
   const profilePath = path.join(getDataPath(lang), "profile.json");
   const profileData = JSON.parse(fs.readFileSync(profilePath, "utf8"));
-  console.log("profileData", profileData);
   
   // Load the language-agnostic skills data
   const skillsPath = path.join(process.cwd(), "src/data", "skills.json");
